637c9a8eec451da2d573b6ad7828b3506b25e1c3
Kavin Aravind <kvnaravind@gmail.com>
2024-06-13 15:23:17 -0400

    pass 3rd stage

--- app/server.go
+++ app/server.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -20,6 +19,27 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	buffer := make([]byte, 4096)
+	conn.Read(buffer)
+
+	lines := strings.Split(string(buffer), "\r\n")
+	if len(lines) == 0 {
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+		return
+	}
+
+	request := strings.Split(lines[0], " ")
+	if len(request) == 0 {
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+		return
+	}
+
+	path := strings.Trim(request[1], "/")
+	if path == "" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 }
